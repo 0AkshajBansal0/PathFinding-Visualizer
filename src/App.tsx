60c131887c0e824377e3d186d17387ce56a166c0
Akshaj Bansal <161148217+0AkshajBansal0@users.noreply.github.com>
2025-02-14 21:50:52 +0530

    pathfinding provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
+import { PathfindingProvider } from "./context/PathfindingContext"
 
 
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline h-screen w-screen bg-blue-500">Hello World!</h1>
+    <PathfindingProvider>
+      <h1 className="text-3xl font-bold underline h-screen w-screen bg-blue-500">Hello World!</h1>
+    </PathfindingProvider>
   )
 }
 
