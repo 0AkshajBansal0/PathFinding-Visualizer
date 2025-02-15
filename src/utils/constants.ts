ba65a7af5ded3f81cdc6c10fb211c5d91cd54556
Akshaj Bansal <161148217+0AkshajBansal0@users.noreply.github.com>
2025-02-16 01:20:05 +0530

    path finding, speed

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,30 +1,30 @@
-import { MazeSelectType } from "./types";
+import { AlgorithmSelectType, MazeSelectType, SpeedSelectType } from "./types";
 
 export const MAX_ROWS = 39;
 export const MAX_COLS = 49;
 
 export const START_TILE_CONFIGURATION = {
-    row: 1,
-    col: 1,
-    isEnd: false,
-    isWall: false,
-    isPath: false,
-    distance: 0,
-    isStart: false,
-    isTraversed: false,
-    parent: null,
+  row: 1,
+  col: 1,
+  isEnd: false,
+  isWall: false,
+  isPath: false,
+  distance: 0,
+  isStart: false,
+  isTraversed: false,
+  parent: null,
 };
 
 export const END_TILE_CONFIGURATION = {
-    row: MAX_ROWS - 2,
-    col: MAX_COLS - 2,
-    isEnd: false,
-    isWall: false,
-    isPath: false,
-    distance: 0,
-    isStart: false,
-    isTraversed: false,
-    parent: null,
+  row: MAX_ROWS - 2,
+  col: MAX_COLS - 2,
+  isEnd: false,
+  isWall: false,
+  isPath: false,
+  distance: 0,
+  isStart: false,
+  isTraversed: false,
+  parent: null,
 };
 
 export const TILE_STYLE =
@@ -40,3 +40,19 @@ export const MAZES: MazeSelectType[] = [
   { name: "Binary Tree", value: "BINARY_TREE" },
   { name: "Recursive Division", value: "RECURSIVE_DIVISION" },
 ];
+
+export const PATHFINDING_ALGORITHMS: AlgorithmSelectType[] = [
+  { name: "Dijkstra", value: "DIJKSTRA" },
+  { name: "A-Star", value: "A_STAR" },
+  { name: "Breath First Search", value: "BFS" },
+  { name: "Depth First Search", value: "DFS" },
+];
+
+export const SPEEDS: SpeedSelectType[] = [
+  { name: "Slow", value: 2 },
+  { name: "Medium", value: 1 },
+  { name: "Fast", value: 0.5 },
+];
+
+export const SLEEP_TIME = 8;
+export const EXTENDED_SLEEP_TIME = 30;
