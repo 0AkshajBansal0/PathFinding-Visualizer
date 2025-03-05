72bb3f7eff50fd25ec84fc19b2c961989dade7c1
Akshaj Bansal <161148217+0AkshajBansal0@users.noreply.github.com>
2025-03-05 16:38:42 +0530

    added nav, footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,28 @@
-import { PathfindingProvider } from "./context/PathfindingContext"
-import { SpeedProvider } from "./context/SpeedContext"
-import { TileProvider } from "./context/TileContext"
-
+import { useRef } from "react";
+import { Grid } from "./components/Grid";
+import { PathfindingProvider } from "./context/PathfindingContext";
+import { SpeedProvider } from "./context/SpeedContext";
+import { TileProvider } from "./context/TileContext";
+import { Nav } from "./components/Nav";
 
 function App() {
+  const isVisualizationRunningRef = useRef(false);
+
   return (
     <PathfindingProvider>
       <TileProvider>
         <SpeedProvider>
-          <h1 className="text-3xl font-bold underline h-screen w-screen bg-blue-500">Hello World!</h1>
+          <div className="h-screen w-screen flex flex-col bg-[#131416]">
+            <Nav isVisualizationRunningRef={isVisualizationRunningRef} />
+            <Grid isVisualizationRunningRef={isVisualizationRunningRef} />
+            <footer className="text-center text-white py-2 bg-[#1a1b1e] mt-auto">
+              <p>© {new Date().getFullYear()} Done by Akshaj Bansal. All rights reserved.</p>
+            </footer>
+          </div>
         </SpeedProvider>
       </TileProvider>
     </PathfindingProvider>
-  )
+  );
 }
 
-export default App
+export default App;
